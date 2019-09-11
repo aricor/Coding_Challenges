3a55eb217568aaf2321efeb7b9fabeecaa38551c
ivanderlich@gmail.com <ivanderlich@gmail.com>
2019-09-11 18:18:04 -0300

    Set the stage to start solveing 10.1

---
+++ Replit/10.1 Weighted Graphs.rb
@@ -0,0 +1,14 @@
+#https://repl.it/student/submissions/7361263
+
+def weighted_graphs(matrix)
+  #Check excercise 4.4 Graphs in order to check how to find the occurrence
+end
+
+p weighted_graphs([
+[0, 0, 1, 3, 0, 0],
+[0, 0, 0, 5, 0, 0],
+[1, 0, 0, 2, 1, 4],
+[3, 5, 2, 0, 7, 0],
+[0, 0, 1, 7, 0, 2],
+[0, 0, 4, 0, 2, 0]])
+# => [0, 2, 1, 1, 2, 2]
