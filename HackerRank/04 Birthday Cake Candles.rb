d5f4a6f41a4428ad982ed59462de8ea1d559c42c
ivanderlich@gmail.com <ivanderlich@gmail.com>
2019-09-30 13:58:49 -0300

    Birthday Cakes

--- HackerRank/04 Birthday Cake Candles.rb
+++ HackerRank/04 Birthday Cake Candles.rb
@@ -1,11 +1,8 @@
 def birthdayCakeCandles(ar)
-    max = ar.max    #find the maximum number
+    max = ar.max
     count = 0
-    ar.each{|x|
-        count += 1 if x == max
-    }
-    count
-    #find how many times that number appears and return that number        
+    ar.each{|x|  count += 1 if x == max   }
+    count    
 end
 
 ar=[3, 2, 1, 3]
